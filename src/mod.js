f747b4e521bd35f0b16c1fa32a54f2fe5f27620f
Toha3673 <61982296+Toha3673@users.noreply.github.com>
2024-03-18 11:02:20 +0300

    Add files via upload

--- src/mod.js
+++ src/mod.js
@@ -2,49 +2,45 @@ const BClass = require("C:/snapshot/project/obj/models/enums/BaseClasses");
 const LTColor = require("C:/snapshot/project/obj/models/spt/logging/LogTextColor");
 
 class unbreakable_keys {
+    CFG;
+    Logger;
+    DataBase;
     postDBLoad(container)
     {
-        let cfg = require("../config/config.json");
-        const logger = container.resolve("WinstonLogger");
-        const data = container.resolve("DatabaseServer");
+        this.CFG = require("../config/config.json");
+        this.Logger = container.resolve("WinstonLogger");
+        this.DataBase = container.resolve("DatabaseServer");
 
-        if (cfg.enabled)
+        const dbTables = this.DataBase.getTables();
+        const dbItems = dbTables.templates.items
+
+        for (const item in dbItems)
         {
-            const dbTables = data.getTables();
-            const dbItems = dbTables.templates.items
+            const itemProps = dbItems[item]._props;
+            if (this.CFG.enable_blacklist && this.CFG.blacklisted_keys.includes(dbItems[item]._id))
+                continue;
 
-            for (const item in dbItems) {
-                const itemProps = dbItems[item]._props;
+            if (dbItems[item]._parent == BClass.BaseClasses.KEY_MECHANICAL) {
+                itemProps.DiscardLimit = -1;
 
-                if (cfg.enable_blacklist && cfg.blacklisted_keys.includes(dbItems[item]._id))
-                    continue;
+                if (this.CFG.unbreakable_keys)
+                    itemProps.MaximumNumberOfUsage = 0;
 
-                if (dbItems[item]._parent == BClass.BaseClasses.KEY_MECHANICAL) {
-                    itemProps.DiscardLimit = -1;
-
-                    if (cfg.unbreakable_keys)
-                        itemProps.MaximumNumberOfUsage = 0;
-
-                    if (cfg.weightless_keys)
-                        itemProps.Weight = 0.0;
-                }
-                if (dbItems[item]._parent == BClass.BaseClasses.KEYCARD) {
-                    itemProps.DiscardLimit = -1;
-
-                    if (cfg.unbreakable_keycards)
-                        itemProps.MaximumNumberOfUsage = 0;
-
-                    if (cfg.weightless_keycards)
-                        itemProps.Weight = 0.0;
-                }
+                if (this.CFG.weightless_keys)
+                    itemProps.Weight = 0.0;
             }
 
-            logger.log(`[unbreakable-keys] has been loaded.`, LTColor.LogTextColor.CYAN);
+            if (dbItems[item]._parent == BClass.BaseClasses.KEYCARD) {
+                itemProps.DiscardLimit = -1;
+
+                if (this.CFG.unbreakable_keycards)
+                    itemProps.MaximumNumberOfUsage = 0;
+
+                if (this.CFG.weightless_keycards)
+                    itemProps.Weight = 0.0;
+            }
         }
-        else
-        {
-            logger.log(`[unbreakable-keys] mod is disabled.`, LTColor.LogTextColor.CYAN);
-        }
+        this.Logger.log("[unbreakable-keys] has been loaded.", LTColor.LogTextColor.CYAN);
     }
 }
 
